a01f0ff45c75ee2a2a79d305d8b3590f48814675
ben.micah.peterson <ben.micah.peterson@gmail.com>
2017-04-28 15:34:03 -0400

    Added Exercise Index View

---
+++ Exercise.Models/ExerciseListItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Models
+{
+    public class ExerciseListItem
+    {
+        public int ExcerciseId { get; set; }
+        public string Type { get; set; }
+        public string Intensity { get; set; }
+        public int Duration { get; set; }
+        public int CaloriesBurned { get; set; }
+
+        [Display(Name="Create")]
+        public DateTimeOffset CreatedUTC { get; set; }
+
+    }
+}
---
+++ Exercise.Web/Controllers/ExerciseController.cs
@@ -0,0 +1,19 @@
+﻿using Exercise.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exercise.Web.Controllers
+{
+    public class ExerciseController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            var model = new ExerciseListItem[0];
+            return View(model);
+        }
+    }
+}
