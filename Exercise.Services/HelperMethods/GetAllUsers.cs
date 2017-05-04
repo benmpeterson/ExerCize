d0cb5bac8798a4493bd72398836d87d6666603c8
ben.micah.peterson <ben.micah.peterson@gmail.com>
2017-05-03 20:04:57 -0400

    Adding Admin Logic

---
+++ Exercise.Services/HelperMethods/GetAllUsers.cs
@@ -0,0 +1,23 @@
+﻿using Exercise.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Services.HelperMethods
+{
+    class GetAllUsers
+    {
+        public static List<string> GetUsers()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var blogs = context.Database.SqlQuery<string>("SELECT Id FROM dbo.ApplicationUser").ToList();
+                return blogs;
+            }
+
+
+        }
+    }
+}
